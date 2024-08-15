c70148f659c874824fe1db3ba19e4677e9f8e836
Razig17 <abdelrazig.sharif1@gmail.com>
2024-08-15 17:32:33 +0300

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function concat that takes two string as arguments and
+returns concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns the concatenated string of two strings.
+    """
+    return str1 + str2
