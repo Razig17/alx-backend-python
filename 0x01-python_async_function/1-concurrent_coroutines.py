9be3de7b79f374fbdc793684361166fc4ba01019
Razig17 <abdelrazig.sharif1@gmail.com>
2024-08-19 16:31:47 +0300

    Task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains an asynchronous function that
+spawn wait_random n times with the specified max_delay.
+"""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> list[float]:
+    """"""
+    delays: list[float] = []
+    for _ in range(n):
+        delays.append(await wait_random(max_delay))
+    return delays
