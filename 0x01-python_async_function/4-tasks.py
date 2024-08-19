dace7f270baf27822ddb0579fc5b8407e2ea4fc6
Razig17 <abdelrazig.sharif1@gmail.com>
2024-08-20 02:35:35 +0300

    Task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module contains an asynchronous function that
+spawn wait_random n times with the specified max_delay.
+"""
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns task_wait_random n times with the specified max_delay"""
+    delays = await asyncio.gather(
+            *list(map(lambda _: task_wait_random(max_delay), range(n))))
+    return sorted(delays)
